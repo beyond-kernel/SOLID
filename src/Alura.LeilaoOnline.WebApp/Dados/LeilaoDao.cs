e413ff6300a351c3df3ca4d6b1bc7c187ce4f49d
Rafael Nogueira <rafanog.linux@hotmail.com>
2021-10-18 19:10:16 -0300

    criada classe DAO para controle de acesso ao banco

---
+++ src/Alura.LeilaoOnline.WebApp/Dados/LeilaoDao.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Alura.LeilaoOnline.WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Alura.LeilaoOnline.WebApp.Dados
+{
+    public class LeilaoDao
+    {
+        AppDbContext _context;
+
+        public LeilaoDao()
+        {
+            _context = new AppDbContext();
+        }
+
+        public IEnumerable<Categoria> BuscarCategorias()
+        {
+            return _context.Categorias.ToList();
+        }
+
+        public IEnumerable<Leilao> BuscarLeiloes()
+        {
+            return _context.Leiloes.Include(l => l.Categoria).ToList();
+        }
+
+
+        public void Incluir(Leilao leilao)
+        {
+            _context.Add(leilao);
+            _context.SaveChanges();
+        }
+
+        public void Excluir(Leilao leilao)
+        {
+            _context.Remove(leilao);
+            _context.SaveChanges();
+        }
+
+        public void Alterar(Leilao leilao)
+        {
+            _context.Update(leilao);
+            _context.SaveChanges();
+        }
+
+        public Leilao BuscarPorId(int id)
+        {
+            return _context.Leiloes.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
